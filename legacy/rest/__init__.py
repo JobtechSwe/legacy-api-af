7a5e0c8a01697452bc3cdc1a09e01e7d72e3e9b3
oleg l <oleg.lyalin@arbetsformedlingen.se>
2019-08-21 16:24:54 +0200

    regex time 00-59

--- legacy/rest/__init__.py
+++ legacy/rest/__init__.py
@@ -37,7 +37,7 @@ yrkesgrupp_query.add_argument('yrkesomradeid', type=int, required=True)
 yrkes_query = reqparse.RequestParser()
 yrkes_query.add_argument('yrkesgruppid', type=int, required=True)
 
-datetime_with_option_seconds = r'^(\d\d\d\d-(0?[1-9]|1[0-2])-(0?[1-9]|[12][0-9]|3[01]) (00|[0-9]|1[0-9]|2[0-3]):([0-9]|[0-5][0-9]):{0,1}([0-9]|[0-5][0-9]){0,1})$'
+datetime_with_option_seconds = r'^(\d\d\d\d-(0?[1-9]|1[0-2])-(0?[1-9]|[12][0-9]|3[01]) (00|[0-9]|1[0-9]|2[0-3]):([0-5][0-9]){0,1}(:[0-5][0-9]){0,1})$'
 legacy_query = reqparse.RequestParser()
 legacy_query.add_argument('landid', type=int)
 legacy_query.add_argument('lanid', type=int)
